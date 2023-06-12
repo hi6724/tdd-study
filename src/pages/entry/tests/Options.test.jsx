80fdec027f2cd62afa413fc6599c62f563f4499a
하훈목 <hunmok1027@gmail.com>
2023-06-12 22:55:44 +0900

    🧪 test: 에러 핸들링 test코드

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -10,3 +10,17 @@ test('displays image for each scoop option from server', async () => {
   const altText = scoopImages.map((el) => el.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('displays image for each toppings option from server', async () => {
+  render(<Options optionType={'toppings'} />);
+
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const altText = images.map((el) => el.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
